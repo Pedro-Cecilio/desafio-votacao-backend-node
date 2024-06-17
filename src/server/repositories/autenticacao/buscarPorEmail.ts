a02985dc0ea9b7b111328b5d5307780a1760decd
Pedro-Cecilio <cardev604@gmail.com>
2024-06-17 18:28:51 -0300

    :recycle: refactor: Incluindo usuário ao buscar autenticacao por email

--- src/server/repositories/autenticacao/buscarPorEmail.ts
+++ src/server/repositories/autenticacao/buscarPorEmail.ts
@@ -1,10 +1,12 @@
-import { Autenticacao } from "@prisma/client"
 import { prisma } from "../../database/database"
 
-export const buscarPorEmail = async (email: string): Promise<Autenticacao | null> => {
+export const buscarPorEmail = async (email: string) => {
     return await prisma.autenticacao.findUnique({
         where: {
             email
+        },
+        include:{
+            usuario: true
         }
     })
 }
