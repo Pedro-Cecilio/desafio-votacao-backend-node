ee2cf3911a095cfb9dc619730d3227ad14461300
Pedro-Cecilio <cardev604@gmail.com>
2024-06-21 13:24:28 -0300

    :recycle: refactor: modificando tipo de retorno ao abrir sessão de votação

--- src/server/shared/services/sessaoVotacao/abrir.ts
+++ src/server/shared/services/sessaoVotacao/abrir.ts
@@ -6,8 +6,10 @@ import { sessaoVotacaoRepository } from "../../../repositories/sessaoVotacao";
 import { PautaCompleta } from "../../interface/pauta/PautaCompleta";
 import { SessaoVotacaoCompleta } from "../../interface/sessaoVotacao/SessaoVotacaoCompleta";
 import { classValidatorValidacoes } from "../../validacoes/classValidator/classValidatorValidacoes";
+import { sessaoVotacaoMapper } from "../../mappers/SessaoVotacaoMapper";
+import { SessaoVotacaoRespostaDto } from "../../dto/sessaoVotacao/SessaoVotacaoRespostaDto";
 
-export const abrir = async (dados: AbrirSessaoDto, usuarioId: number): Promise<SessaoVotacaoCompleta> =>{
+export const abrir = async (dados: AbrirSessaoDto, usuarioId: number): Promise<SessaoVotacaoRespostaDto> =>{
     await classValidatorValidacoes.validarDto(dados);
     
     const pauta: PautaCompleta = await pautaService.buscarPorIdEUsuarioId(dados.pautaId, usuarioId)
@@ -17,5 +19,6 @@ export const abrir = async (dados: AbrirSessaoDto, usuarioId: number): Promise<S
     const dataAbertura: Date = dataAtual.clone().toDate();
     const dataFechamento: Date = dataAtual.clone().add(dados.minutos, "minute").toDate();
 
-    return await sessaoVotacaoRepository.abrir(pauta, dataAbertura, dataFechamento);
+    const sessaoVotacao: SessaoVotacaoCompleta = await sessaoVotacaoRepository.abrir(pauta, dataAbertura, dataFechamento);
+    return sessaoVotacaoMapper.sessaoVotacaoCompletaToSessaoVotacaoRespostaDto(sessaoVotacao)!
 }       
